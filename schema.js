9a158ffbdd92ec2ad8a62475f315aa205dd7d4ea
shivamRoy04 <22051888@kiit.ac.in>
2025-07-01 03:08:17 +0530

    fixed map

--- schema.js
+++ schema.js
@@ -1,23 +1,21 @@
 const Joi = require("joi");
 
-module.exports.listingSchema = Joi.object({
+const listingSchema = Joi.object({
   listing: Joi.object({
     title: Joi.string().required(),
     description: Joi.string().required(),
+    price: Joi.number().required().min(0),
     location: Joi.string().required(),
     country: Joi.string().required(),
-    price: Joi.number().required().min(0),
-    image: Joi.object({
-      url: Joi.string().uri().required(),
-      filename: Joi.string().required()
-    }).required()
+    image: Joi.any() // Allow any file input for multer
   }).required()
 });
 
-
-module.exports.reviewSchema = Joi.object({
-   review : Joi.object({
-    rating : Joi.number().required().min(1).max(5),
-    comment : Joi.string().required(),
-   }).required()
+const reviewSchema = Joi.object({
+  review: Joi.object({
+    rating: Joi.number().required().min(1).max(5),
+    comment: Joi.string().required()
+  }).required()
 });
+
+module.exports = { listingSchema, reviewSchema };
